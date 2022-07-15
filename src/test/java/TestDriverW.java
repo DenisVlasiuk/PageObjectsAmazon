99b3ab75085869472f16c8a304d5c391a088ec00
DenisVlasiuk <fcdenisvlasiuk@gmail.com>
2022-07-15 13:10:08 +0300

    Initial commit

---
+++ src/test/java/TestDriverW.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.asn1.sec.SECObjectIdentifiers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestDriverW {
+
+    @Test
+    public void runGoog(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.google.com.ua/");
+        driver.findElement(By.xpath("//input[@name= 'q']")).sendKeys("dog\n");
+        driver.findElement(By.xpath("//h3[contains(text(), 'Wikipedia')]")).click();
+        driver.findElement(By.xpath("//a[contains(@title, 'Evolution')]")).click();
+
+
+
+
+
+    }
+
+}
